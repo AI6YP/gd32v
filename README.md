b1863f29bd8fa1795953ef1fdaf2e08d3383e6bf
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2022-06-28 14:53:35 +0300

    readme merge

--- README.md
+++ README.md
@@ -10,3 +10,31 @@ using LLVM 11.0.1
 sudo cp 11-ftdi.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && udevadm trigger
 ```
+
+https://habr.com/ru/post/556498/
+
+https://www.appelsiini.net/2020/programming-gd32v-longan-nano/
+
+https://github.com/riscv-rust/seedstudio-gd32v
+
+https://www.riscv-mcu.com/
+
+https://www.rvmcu.com/uploadfile/pdf/0/0/239.pdf
+
+https://github.com/sipeed/platform-gd32v/issues/2
+
+https://github.com/riscv/riscv-openocd/pull/518
+
+https://youtu.be/ogbutL6C9oI
+
+http://microsin.net/programming/arm/configuring-openocd-for-the-at91sam7se.html
+
+https://dzone.com/articles/jtag-debugging-the-esp32-with-ft2232-and-openocd
+
+https://www.allaboutcircuits.com/technical-articles/getting-started-with-openocd-using-ft2232h-adapter-for-swd-debugging/
+
+https://mcuoneclipse.com/2019/10/20/jtag-debugging-the-esp32-with-ft2232-and-openocd/
+
+https://balau82.wordpress.com/2013/08/04/jtag-connection-with-openocd-and-ftdi-cable/
+
+https://gist.github.com/elfmimi/1deb9c94b0f0900ae8a9df740b62bcd6
--- blink/main.c
+++ blink/main.c
@@ -1,23 +1,23 @@
 #include <string.h>
-#include "gd32vf103.h"
+#include "gd32vf103xb.h"
 
 // Pre-defined memory locations for program initialization.
 extern uint32_t _sidata, _sdata, _edata, _sbss, _ebss;
 
-// Simple 'busy loop' delay method.
-void delay_cycles( uint32_t cyc ) {
-  uint32_t d_i;
-  for ( d_i = 0; d_i < cyc; ++d_i ) {
-    __asm__( "nop" );
+void delay_ms(uint32_t d) {
+  for (uint32_t i = 0; i < (d * 1300); i++) {
+    __asm__( "nop; nop; nop" );
   }
 }
 
-// 'main' method which gets called from the boot code.
+// Longan Nano / LILYGO
+// PC13 -- red   led
+// PA1  -- green led
+// PA2  -- blue  led
+
 int main () {
-  // Copy initialized data from .sidata (Flash) to .data (RAM)
-  memcpy( &_sdata, &_sidata, ( ( void* )&_edata - ( void* )&_sdata ) );
-  // Clear the .bss RAM section.
-  memset( &_sbss, 0x00, ( ( void* )&_ebss - ( void* )&_sbss ) );
+  memcpy( &_sdata, &_sidata, ( ( void* )&_edata - ( void* )&_sdata ) ); // copy Flash -> RAM
+  memset( &_sbss, 0x00, ( ( void* )&_ebss - ( void* )&_sbss ) ); // Clear .bss RAM
 
   RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 
@@ -30,10 +30,10 @@ int main () {
   GPIOA->CRL |= (0x2 << GPIO_CRL_MODE2_Pos);
 
   while(1) {
-    GPIOA->ODR = 2; // blue LED on
-    delay_cycles(300000);
-    GPIOA->ODR = 6; // blue LED off
-    delay_cycles(2000000);
+    for (int i = 0; i < 4; i++) {
+      GPIOA->ODR = i << 1; // green / blue mix
+      delay_ms(1000);
+    }
   }
   return 0;
 }
